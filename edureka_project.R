025507d4b5065e832aa2502c310fbf9385f49625
Olufemi George <olufemig@outlook.com>
2017-04-09 12:20:38 +0100

    eda completed

--- edureka_project.R
+++ edureka_project.R
@@ -1,37 +1,29 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(corrplot)
+library(caret)
 df.retail <- read.csv("Retail_Case_Study_Data.csv")
-summary(df.retail)
+head(df.retail)
 #there are no nulls
-str(df.retail)
+summary(df.retail)
 table(df.retail$Purchase.Channel)
-#tail(df.retail)
-# mens and women merchandise, new customer and visited website and sale made are factors
-# and needs to be converted
-#df.retail$Mens.Merchandise <- as.factor(df.retail$Mens.Merchandise)
-#df.retail$Womens.Merchandise <- as.factor(df.retail$Womens.Merchandise)
-#df.retail$New.Customer <- as.factor(df.retail$New.Customer)
-#df.retail$Visited.Website <- as.factor(df.retail$Visited.Website)
-#df.retail$Sale.Made <- as.factor(df.retail$Sale.Made)
-#dependent variable needs to be integer
-#df.retail$Sale.Made<- as.numeric(as.character(df.retail$Sale.Made))
 #take out the customer id and spend category
 df.retail = subset(df.retail, select = -c(1,3))
-str(df.retail)
 # split input and output 
-x <- df.retail[,1:4] 
-y <- df.retail[,9]
-#boxplot for all dependent variables
-par(mfrow=c(1,4)) 
-for(i in 1:4) {
-boxplot(x[,i], main=names(df.retail)[i]) }
-barplot(df.retail$Mens.Merchandise)
-hist(df.retail$Mens.Merchandise)
-table(df.retail$Area)
+#x <- df.retail[,1:4] 
+#y <- df.retail[,9]
+pairs(df.retail[,-c(5,7)])
+# very little correlation between the numeric variables
+str(df.retail)
 barplot(xtabs(~df.retail$Area))
-hist(df.retail$Spend.Numeric)
+# most users are suburban which means they maybe based where the stores are located
+ggplot(df.retail, aes(x=Spend.Numeric)) +
+  geom_histogram(binwidth = 150)
 boxplot(df.retail$Spend.Numeric, horizontal = TRUE)
 barplot(table(df.retail$Area))
 plot(Spend.Numeric~Purchase.Channel, data=df.retail)
 pairs(df.retail[,c(1,2,5,6,7)])
+table(df.retail$Area)
+plot(df.retail$Area ~ df.retail$Spend.Numeric)
+# next is data modelling
