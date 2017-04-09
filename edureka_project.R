14b4156225db2ec83cf87bebd87534b3d68ed253
Olufemi George <olufemig@outlook.com>
2017-04-09 23:44:28 +0100

    added accuracy

--- edureka_project.R
+++ edureka_project.R
@@ -5,6 +5,10 @@ library(corrplot)
 library(caret)
 df.retail <- read.csv("Retail_Case_Study_Data.csv")
 df.retail$Sale.Made<- as.factor(df.retail$Sale.Made)
+df.retail$Mens.Merchandise<- as.factor(df.retail$Mens.Merchandise)
+df.retail$Womens.Merchandise<- as.factor(df.retail$Womens.Merchandise)
+df.retail$New.Customer<- as.factor(df.retail$New.Customer)
+df.retail$Visited.Website<- as.factor(df.retail$Visited.Website)
 #head(df.retail)
 #there are no nulls
 str(df.retail)
@@ -22,11 +26,11 @@ barplot(xtabs(~df.retail$Area))
 ggplot(df.retail, aes(x=Spend.Numeric)) +
   geom_histogram(binwidth = 150)
 boxplot(df.retail$Spend.Numeric, horizontal = TRUE)
-barplot(table(df.retail$Area))
-plot(Spend.Numeric~Purchase.Channel, data=df.retail)
+#barplot(table(df.retail$Area))
+#plot(Spend.Numeric~Purchase.Channel, data=df.retail)
 pairs(df.retail[,c(1,2,5,6,7)])
-table(df.retail$Area)
-plot(df.retail$Area ~ df.retail$Spend.Numeric)
+#table(df.retail$Area)
+#plot(df.retail$Area ~ df.retail$Spend.Numeric)
 # next is data modelling
 #split the data into training and test
 # create a list of 80% of the rows in the original dataset we can use for training 
@@ -39,18 +43,28 @@ str(df.mydata)
 #create validation harness
 trainControl <- trainControl(method="cv", number=10)
 metric <- "Accuracy"
+# LG 
+set.seed(7) 
+fit.lg <- train(Sale.Made~., data=df.mydata, method="glm", metric=metric,preProc=c("center", "scale"), trControl=trainControl) 
+# GLMNET
+set.seed(7) 
+fit.glmnet <- train(Sale.Made~., data=df.mydata, method="glmnet", metric=metric,preProc=c("center", "scale"),  trControl=trainControl) 
 # LDA 
 set.seed(7) 
-fit.lda <- train(Sale.Made~., data=df.mydata, method="lda", metric=metric, trControl=trainControl) 
+fit.lda <- train(Sale.Made~., data=df.mydata, method="lda", metric=metric,preProc=c("center", "scale"),  trControl=trainControl) 
 # CART 
 set.seed(7) 
-fit.cart <- train(Sale.Made~., data=df.mydata, method="rpart", metric=metric, trControl=trainControl) 
+fit.cart <- train(Sale.Made~., data=df.mydata, method="rpart", metric=metric,preProc=c("center", "scale"),  trControl=trainControl) 
 # KNN 
 set.seed(7) 
-fit.knn <- train(Sale.Made~., data=df.mydata, method="knn", metric=metric, trControl=trainControl) 
-# SVM 
+fit.knn <- train(Sale.Made~., data=df.mydata, method="knn", metric=metric,preProc=c("center", "scale"),  trControl=trainControl) 
+# NB 
 set.seed(7) 
-fit.svm <- train(Sale.Made~., data=df.mydata, method="svmRadial", metric=metric, trControl=trainControl) 
-# Random Forest 
+fit.nb <- train(Sale.Made~., data=df.mydata, method="nb", metric=metric, trControl=trainControl) 
+# CART 
 set.seed(7) 
-fit.rf <- train(Sale.Made~., data=df.mydata, method="rf", metric=metric, trControl=trainControl)
+fit.cart <- train(Sale.Made~., data=df.mydata, method="rpart", metric=metric, trControl=trainControl)
+# Compare algorithms 
+results <- resamples(list(LG=fit.glm, LDA=fit.lda, GLMNET=fit.glmnet, KNN=fit.knn, CART=fit.cart, NB=fit.nb)) 
+summary(results) 
+dotplot(results)
