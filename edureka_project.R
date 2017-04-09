fb779243907024f7fa72cb0413eceaecb73073b1
Olufemi George <olufemig@outlook.com>
2017-04-09 23:05:49 +0100

    added modelling script

--- edureka_project.R
+++ edureka_project.R
@@ -4,9 +4,10 @@ library(ggplot2)
 library(corrplot)
 library(caret)
 df.retail <- read.csv("Retail_Case_Study_Data.csv")
-head(df.retail)
+df.retail$Sale.Made<- as.factor(df.retail$Sale.Made)
+#head(df.retail)
 #there are no nulls
-summary(df.retail)
+str(df.retail)
 table(df.retail$Purchase.Channel)
 #take out the customer id and spend category
 df.retail = subset(df.retail, select = -c(1,3))
@@ -29,11 +30,27 @@ plot(df.retail$Area ~ df.retail$Spend.Numeric)
 # next is data modelling
 #split the data into training and test
 # create a list of 80% of the rows in the original dataset we can use for training 
-validationIndex <- createDataPartition(dataset$Species, p=0.80, list=FALSE) 
+validationIndex <- createDataPartition(df.retail$Sale.Made, p=0.80, list=FALSE) 
 # select 20% of the data for validation 
-validation <- dataset[-validationIndex,] 
+validation <- df.retail[-validationIndex,] 
 # use the remaining 80% of data to training and testing the models 
-dataset <- dataset[validationIndex,]
+df.mydata <- df.retail[validationIndex,]
+str(df.mydata)
 #create validation harness
 trainControl <- trainControl(method="cv", number=10)
 metric <- "Accuracy"
+# LDA 
+set.seed(7) 
+fit.lda <- train(Sale.Made~., data=df.mydata, method="lda", metric=metric, trControl=trainControl) 
+# CART 
+set.seed(7) 
+fit.cart <- train(Sale.Made~., data=df.mydata, method="rpart", metric=metric, trControl=trainControl) 
+# KNN 
+set.seed(7) 
+fit.knn <- train(Sale.Made~., data=df.mydata, method="knn", metric=metric, trControl=trainControl) 
+# SVM 
+set.seed(7) 
+fit.svm <- train(Sale.Made~., data=df.mydata, method="svmRadial", metric=metric, trControl=trainControl) 
+# Random Forest 
+set.seed(7) 
+fit.rf <- train(Sale.Made~., data=df.mydata, method="rf", metric=metric, trControl=trainControl)
