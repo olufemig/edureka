272384b04f1e0406c2fada2453dcf4c495996d74
Olufemi George <olufemig@outlook.com>
2017-04-18 23:21:13 +0100

    corrected initial dataset

--- edureka_project.R
+++ edureka_project.R
@@ -4,33 +4,23 @@ library(ggplot2)
 library(corrplot)
 library(caret)
 df.retail <- read.csv("Retail_Case_Study_Data.csv")
-df.retail$Sale.Made<- as.factor(df.retail$Sale.Made)
-#df.retail$Mens.Merchandise<- as.factor(df.retail$Mens.Merchandise)
-#df.retail$Womens.Merchandise<- as.factor(df.retail$Womens.Merchandise)
-#df.retail$New.Customer<- as.factor(df.retail$New.Customer)
-#df.retail$Visited.Website<- as.factor(df.retail$Visited.Website)
-#head(df.retail)
-#there are no nulls
 str(df.retail)
 table(df.retail$Purchase.Channel)
-#take out the customer id and spend category
-df.retail = subset(df.retail, select = -c(1,3))
-# split input and output 
-#x <- df.retail[,1:4] 
-#y <- df.retail[,9]
-pairs(df.retail[,-c(5,7)])
-# very little correlation between the numeric variables
+#take out the customer id 
+df.retail = df.retail[,-1]
+#convert spend category to factor
+df.retail$Spend.Category<-as.factor(df.retail$Spend.Category)
 str(df.retail)
+#see if any correlation between numeric variables
+pairs(df.retail[,-c(2,6,8)])
+# univariate analysis
 barplot(xtabs(~df.retail$Area))
 # most users are suburban which means they maybe based where the stores are located
 ggplot(df.retail, aes(x=Spend.Numeric)) +
  geom_histogram(binwidth = 150)
 boxplot(df.retail$Spend.Numeric, horizontal = TRUE)
-#barplot(table(df.retail$Area))
-#plot(Spend.Numeric~Purchase.Channel, data=df.retail)
-pairs(df.retail[,c(1,2,5,6,7)])
-#table(df.retail$Area)
-#plot(df.retail$Area ~ df.retail$Spend.Numeric)
+#side by side boxplot
+boxplot(df.retail$Spend.Numeric~ df.retail$Area)
 # next is data modelling
 #split the data into training and test
 # create a list of 80% of the rows in the original dataset we can use for training 
@@ -67,7 +57,6 @@ fit.cart <- train(Sale.Made~., data=df.mydata, method="rpart", metric=metric, tr
 # Compare algorithms 
 results <- resamples(list(LG=fit.glm, LDA=fit.lda, GLMNET=fit.glmnet, KNN=fit.knn, CART=fit.cart, NB=fit.nb)) 
 summary(results) 
-dotplot(results)
-pred <- predict(fit.glm, type ='raw',newdata = validation[-9])
-table(pred)
+pred <- predict(fit.glm,newdata = validation[-9],type ="response")
 summary(fit.glm)
+head( validation[-9])
